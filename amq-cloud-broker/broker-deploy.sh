1d6a628dd1591f0fdce53e457baa7ff7eb4c11ae
Josh West <shuawest@gmail.com>
2020-04-07 17:10:46 -0400

    correcting jolokia connectivity

--- amq-cloud-broker/broker-deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-export DYLD_LIBRARY_PATH=/usr/local/opt/openssl/lib:$DYLD_LIBRARY_PATH
-
-echo; echo "~~~ Deploying Broker via Operator ~~~"; echo;
-ansible-playbook -i inventories/rhpds.ini playbooks/deploy_broker_operator.yml -vvv
- 
